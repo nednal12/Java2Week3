9d84ffba5528effe31b4c79247aae2e9c5e792db
Brent Marohnic <nednal12@fullsail.edu>
2013-09-19 23:14:25 -0400

    Everything's working, final commit

--- src/com/bmarohnic/java2week3/NextActivity.java
+++ src/com/bmarohnic/java2week3/NextActivity.java
@@ -3,14 +3,38 @@ package com.bmarohnic.java2week3;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class NextActivity extends Activity{
 
+	TextView textView;
+	EditText editText;
+	String dealZip = null;
+	int dealCategory;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.activity_next);
+		
+		String dealType = null;
+		
+		Log.i("onCreate", "This is being called");
+		Bundle data = getIntent().getExtras();
+		if(data != null)
+		{
+			dealType = data.getString("dealType");
+			dealCategory = data.getInt("dealCategory");
+			
+		}
+		
+		textView = (TextView) findViewById(R.id.dealTypeTextView);
+		textView.setText("Enter the Zip Code to find " + dealType + " in that area.");
+		
 		
 	}
 
@@ -24,10 +48,18 @@ public class NextActivity extends Activity{
 	@Override
 	public void finish() {
 	    Intent data = new Intent();
-	    data.putExtra("keyA", "Value for keyA");
-	    data.putExtra("keyB", "Value for keyB");
+	    data.putExtra("dealZip", dealZip);
+	    data.putExtra("dealCategory", dealCategory);
 	    setResult(RESULT_OK, data);
 	    super.finish();
 	}
 	
+	
+	public void onClick(View view)
+	{
+		editText = (EditText) findViewById(R.id.searchField);
+		dealZip = editText.getText().toString();
+		
+		finish();
+	}
 }
