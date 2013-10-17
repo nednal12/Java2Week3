1f3de41f849a0bc1957887c9aa3a3b933e12b1c4
Brent Marohnic <nednal12@fullsail.edu>
2013-10-17 19:44:56 -0400

    Everything works

--- gen/com/bmarohnic/java2week3/R.java
+++ gen/com/bmarohnic/java2week3/R.java
@@ -26,10 +26,12 @@ public final class R {
     public static final class drawable {
         public static final int backrepeat=0x7f020000;
         public static final int dollarsign=0x7f020001;
-        public static final int ic_launcher=0x7f020002;
+        public static final int ic_action_web_site=0x7f020002;
+        public static final int ic_launcher=0x7f020003;
     }
     public static final class id {
-        public static final int action_settings=0x7f09000f;
+        public static final int action_search=0x7f09000f;
+        public static final int action_web=0x7f090010;
         public static final int city=0x7f09000e;
         public static final int city_header=0x7f09000b;
         public static final int dealSpinner=0x7f090002;
@@ -56,11 +58,13 @@ public final class R {
     }
     public static final class menu {
         public static final int main=0x7f080000;
+        public static final int main_activity_actions=0x7f080001;
     }
     public static final class string {
         public static final int action_settings=0x7f050002;
         public static final int app_name=0x7f050000;
         public static final int app_name2=0x7f050001;
+        public static final int eight_coupons=0x7f05000d;
         public static final int enter_your_uri=0x7f050009;
         public static final int enter_your_zip_code=0x7f050007;
         public static final int go=0x7f050008;
@@ -69,6 +73,7 @@ public final class R {
         public static final int launchImplicitButton=0x7f05000c;
         public static final int launchSearchButton=0x7f05000b;
         public static final int searchForDeals=0x7f050005;
+        public static final int search_by_zip=0x7f05000e;
         public static final int selectCategory=0x7f050004;
         public static final int uri=0x7f05000a;
     }
--- src/com/bmarohnic/java2week3/LaunchScreen.java
+++ src/com/bmarohnic/java2week3/LaunchScreen.java
@@ -5,6 +5,8 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 
 
@@ -17,7 +19,7 @@ public class LaunchScreen extends Activity{
 		
 		setContentView(R.layout.activity_launch);
 	}
-
+  
 	/* (non-Javadoc)
 	 * @see android.app.Activity#onCreateOptionsMenu(android.view.Menu)
 	 */
@@ -25,9 +27,31 @@ public class LaunchScreen extends Activity{
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
+//		MenuInflater inflater = getMenuInflater();
+//	    inflater.inflate(R.menu.main_activity_actions, menu);
 		return true;
 	}
 	
+	
+	   
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// TODO Auto-generated method stub
+		switch (item.getItemId())
+		{
+        case R.id.action_web:
+        	Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://8coupons.com"));
+    		startActivity(intent);
+            return true;
+        case R.id.action_search:
+        	Intent mainActivity = new Intent(this, MainActivity.class);
+    		startActivity(mainActivity);
+            return true;
+        default:
+            return super.onOptionsItemSelected(item);
+		}
+	}
+
 	public void searchClick(View view) {
 
 		Intent mainActivity = new Intent(this, MainActivity.class);
